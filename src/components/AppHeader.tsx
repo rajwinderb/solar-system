83096755c05008a2ac816bffcfbbb14175f24f0d
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2021-11-09 16:56:41 +0000

    basic structure added

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,7 @@
+export default function AppHeader(): JSX.Element {
+  return (
+    <header className="AppHeader">
+      <h1>Solar System</h1>
+    </header>
+  );
+}
---
+++ src/components/SolarSystem.tsx
@@ -0,0 +1,7 @@
+export default function SolarSystem(): JSX.Element {
+  return (
+    <section className="SolarSystem">
+      <p>solar system</p>
+    </section>
+  );
+}
