af1fc7c56ddf167a1ec5ac742b57ff2d441429bc
rajwinderb <68147202+rajwinderb@users.noreply.github.com>
2022-01-09 21:04:43 +0000

    add facts from API to planet modals

--- src/components/PlanetModal.tsx
+++ src/components/PlanetModal.tsx
@@ -33,14 +33,39 @@ export default function PlanetModal(props: IPlanetModal): JSX.Element {
   const ModalContent = styled.div`
     display: flex;
     flex-direction: column;
-    line-height: 1.8;
+    line-height: 1;
     color: #fff;
     h2 {
       text-align: center;
       font-family: "Press Start 2P", cursive;
     }
     p {
-      margin-bottom: 1rem;
+      text-align: left;
+      margin-top: 5px;
+      margin-bottom: 5px;
+      margin-left: 25px;
+    }
+    .facts {
+      text-align: center;
+      text-decoration-line: underline;
+      margin-top: 20px;
+      margin-bottom: 15px;
+    }
+    .factsList {
+      text-align: left;
+      list-style-type: disc;
+      list-style-position: inside;
+      display: inline;
+      height: 100%;
+      overflow: hidden;
+    }
+    .factsList li {
+      text-decoration-line: none;
+      margin-bottom: 5px;
+    }
+    .factsList li:hover {
+      cursor: auto;
+      color: #fff;
     }
   `;
 
@@ -71,6 +96,59 @@ export default function PlanetModal(props: IPlanetModal): JSX.Element {
             <ModalContent>
               <CloseModalButton aria-label="Close modal" onClick={closeModal} />
               <h2>{props.planet.name}</h2>
+              <p>
+                <strong>Distance from sun: </strong>
+                {props.planet.distance_from_sun.toLocaleString()} km
+              </p>
+              <p>
+                <strong>Diameter: </strong>
+                {props.planet.distance_from_sun.toLocaleString()} km
+              </p>
+              <p>
+                <strong>Moons: </strong>
+                {props.planet.moons}
+              </p>
+              {props.planet.name !== "Earth" ? (
+                <p>
+                  <strong>Length of year: </strong>
+                  {props.planet.length_of_year} Earth years
+                </p>
+              ) : (
+                <p>
+                  <strong>Length of year: </strong>
+                  {props.planet.length_of_year} days
+                </p>
+              )}
+              {props.planet.avg_temp !== null && (
+                <p>
+                  <strong>Average Temperature: </strong>
+                  {props.planet.avg_temp}°C
+                </p>
+              )}
+              {props.planet.min_temp !== null && props.planet.max_temp && (
+                <p>
+                  <strong>Surface Temperature Range: </strong>
+                  Between {props.planet.min_temp}°C and {props.planet.min_temp}
+                  °C
+                </p>
+              )}
+              {props.planet.first_record !== null && props.planet.recorded_by && (
+                <p>
+                  <strong>First Recorded: </strong>
+                  {props.planet.first_record} by {props.planet.recorded_by}
+                </p>
+              )}
+              <p className="facts">
+                <strong>Fun Facts</strong>
+              </p>
+              <ul className="factsList">
+                {props.planet.facts.split(". ").map((fact, index) => (
+                  <li key={index}>
+                    {fact.trim()}
+                    {fact.trim().slice(-1) !== "." && "."}
+                  </li>
+                ))}
+              </ul>
             </ModalContent>
           </ModalWrapper>
         </Background>
